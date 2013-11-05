f98d01dedd48e1b74d06f3fee92761f6a2223f9d
Peter Northup <peter.northup@gmail.com>
2013-11-05 15:25:36 -0800

    Initial commit.

---
+++ Gemfile
@@ -0,0 +1,28 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.13'
+
+gem 'sqlite3'
+gem 'pg'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :test, :development do
+  gem 'awesome_print'
+  gem 'debugger'
+  gem 'better_errors'
+  gem 'meta_request'
+  gem 'binding_of_caller'
+  gem 'quiet_assets'
+  gem 'rails-backbone'
+  gem 'twitter-bootstrap-rails'
+end
+
+gem 'jquery-rails'
+
+gem 'bcrypt-ruby', '~> 3.0.0'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GPXtra::Application.config.secret_token = 'a54a3236c02f80797f57ac50d274504498dee73a1414262878f5ecf6cf60ff73beafa6ef03a7524251c603a2ea8cc151428e73a00ec55d41304407fbeac1278d'
